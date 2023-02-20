8c1adc9b4e688af99fedab1511913128fa29aa8c
JavaThorman <120687540+JavaThorman@users.noreply.github.com>
2023-02-20 09:41:17 +0100

    init

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,34 @@
-public class ArraysOchListor {
+public class Main {
     public static void main(String[] args) {
 
+        // Array[] - lista av värden som ligger i rad i minnet, statisk storlek.
+        // Olika listor - ArrayList(), Hashet(), Hashmap(), LinkedList().
+
+        // En array ar en rad med data som ligger i minne.
+        // int[] nuffror1 = new int[3];
+        // int[] nuffror2 = {1, 2, 3};
+
+         int[] nuffror1 = new int[3];
+        nuffror1[0]=13;
+        nuffror1[1]=37;
+        nuffror1[2]=1337;
+
+        int[] nuffror2 = {1,2,3};
+        System.out.println(nuffror2[1]);
+        // ---------- LISTOR ----------
+        // Listor växer dynamiskt, arrayer är statiskt stora.
+        //ArrayList() - en dynamisk samling av objekt som kan växa eller krympa efter behov
+          //      Hashet() - Datastruktur i Java som används för att lagra unika element utan att tillåta () -> dubletter
+            //    Hashmap() - Datastruktur i java som tillåter lagring av värden i par av nycklar och värden.
+
+        //LinkedList() - Datastruktur som består av en kedja av noder där varje nod har en referens till nästa nod och som
+        // möjliggör dynamiskt läggande tilloch borttagande av element.
+        // ---------- ----------
 
 
 
-
-
-
+// C.R.U.D (L)
+        // Crud även kallad CRUDL är ett mänster man alltid följer när man arbetar med datamängder.
+        // C - create, R -read, U - update, D - Delete, L - List (brukar klassifieras i R delen.
     }
 }
